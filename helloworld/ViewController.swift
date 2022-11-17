2a9c833bcf078090661c424fef0f857e1444e13f
kurdiahmad1996 <a.kurdi@livaatverse.com>
2022-11-17 16:41:34 +0300

    first

---
+++ helloworld/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Obaida on 11/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbl: UILabel!
+    override func viewDidLoad() {
+        
+        lbl.text="hello world"
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
